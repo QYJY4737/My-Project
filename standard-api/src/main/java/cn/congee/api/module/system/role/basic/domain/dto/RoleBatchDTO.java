09e06a5e535ebe82d339a43382feeef8d3ed577d
QYJY4737 <qyjy4737@163.com>
2021-01-05 20:33:20 +0800

    2021-1-5-20:33 修改RoleBatchDTO

--- standard-api/src/main/java/cn/congee/api/module/system/role/basic/domain/dto/RoleBatchDTO.java
+++ standard-api/src/main/java/cn/congee/api/module/system/role/basic/domain/dto/RoleBatchDTO.java
@@ -21,9 +21,6 @@ public class RoleBatchDTO {
     @NotNull(message = "角色id不能为空")
     protected Long roleId;
 
-    /**
-     * 员工id集合
-     */
     @ApiModelProperty(value = "员工id集合")
     @NotEmpty(message = "员工id不能为空")
     protected List<Long> employeeIds;
