d2e0c99643d69af53443359f41beb4038601fb6b
QYJY4737 <qyjy4737@163.com>
2021-01-05 20:15:39 +0800

    2021-1-5-20:15 修改RolePrivilegeDTO

--- standard-api/src/main/java/cn/congee/api/module/system/role/roleprivilege/domain/dto/RolePrivilegeDTO.java
+++ standard-api/src/main/java/cn/congee/api/module/system/role/roleprivilege/domain/dto/RolePrivilegeDTO.java
@@ -14,16 +14,10 @@ import java.util.List;
 @Data
 public class RolePrivilegeDTO {
 
-    /**
-     * 角色id
-     */
     @ApiModelProperty("角色id")
     @NotNull(message = "角色id不能为空")
     private Long roleId;
 
-    /**
-     * 功能权限id 集合
-     */
     @ApiModelProperty("功能权限Key集合")
     @NotNull(message = "功能权限集合不能为空")
     private List<String> privilegeKeyList;
