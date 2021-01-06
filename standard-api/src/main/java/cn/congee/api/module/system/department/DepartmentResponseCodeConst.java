611ecffaf96b59e130645b97efff80ee1a75bbcb
QYJY4737 <qyjy4737@163.com>
2021-01-06 10:17:59 +0800

    2021-1-6-10:17 修改部门返回信息常量类

--- standard-api/src/main/java/cn/congee/api/module/system/department/DepartmentResponseCodeConst.java
+++ standard-api/src/main/java/cn/congee/api/module/system/department/DepartmentResponseCodeConst.java
@@ -20,17 +20,15 @@ public class DepartmentResponseCodeConst extends ResponseCodeConst {
     /**
      * 当前部门有子级部门 不能删除 10003
      */
-    public static final DepartmentResponseCodeConst CANNOT_DEL_DEPARTMENT_WITH_CHILD = new
-            DepartmentResponseCodeConst(2002, "当前部门有子级部门,无法删除！");
+    public static final DepartmentResponseCodeConst CANNOT_DEL_DEPARTMENT_WITH_CHILD = new DepartmentResponseCodeConst(2002, "当前部门有子级部门,无法删除！");
 
     /**
      * 当前部门有员工 不能删除 10004
      */
-    public static final DepartmentResponseCodeConst CANNOT_DEL_DEPARTMENT_WITH_EMPLOYEE = new
-            DepartmentResponseCodeConst(2003, "当前部门有员工,无法删除！");
+    public static final DepartmentResponseCodeConst CANNOT_DEL_DEPARTMENT_WITH_EMPLOYEE = new DepartmentResponseCodeConst(2003, "当前部门有员工,无法删除！");
 
     /**
-     *
+     * 上级部门id不能等于当前部门id 10005
      */
     public static final DepartmentResponseCodeConst PARENT_ID_ERROR = new DepartmentResponseCodeConst(2004, "上级部门id不能等于当前部门id");
 
