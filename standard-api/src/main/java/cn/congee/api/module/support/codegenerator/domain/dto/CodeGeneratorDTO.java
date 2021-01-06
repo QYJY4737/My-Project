a3c1ae20644f51f25b79d7e58bfe0db65e423073
QYJY4737 <qyjy4737@163.com>
2021-01-06 12:17:21 +0800

    2021-1-6-12:17 修改CodeGeneratorDTO

--- standard-api/src/main/java/cn/congee/api/module/support/codegenerator/domain/dto/CodeGeneratorDTO.java
+++ standard-api/src/main/java/cn/congee/api/module/support/codegenerator/domain/dto/CodeGeneratorDTO.java
@@ -1,5 +1,6 @@
-package cn.congee.api.module.support.codegenerator.domain;
+package cn.congee.api.module.support.codegenerator.domain.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -14,39 +15,25 @@ import java.util.List;
 @Builder
 public class CodeGeneratorDTO {
 
-    /**
-     * 需要生成代码的表名
-     */
+    @ApiModelProperty("需要生成代码的表名")
     private String tableName;
 
-    /**
-     * 表前缀
-     */
+    @ApiModelProperty("表前缀")
     private String tablePrefix;
 
-    /**
-     * 基础包路径
-     */
+    @ApiModelProperty("基础包路径")
     private String basePackage;
 
-    /**
-     * module下的子包
-     */
+    @ApiModelProperty("module下的子包")
     private String modulePackage;
 
-    /**
-     * 公司
-     */
+    @ApiModelProperty("公司")
     private String company;
 
-    /**
-     * 作者
-     */
+    @ApiModelProperty("作者")
     private String author;
 
-    /**
-     * 需要构建查询方法的列
-     */
+    @ApiModelProperty("需要构建查询方法的列")
     private List<CodeGeneratorQueryColumnDTO> queryColumnList;
 
 }
